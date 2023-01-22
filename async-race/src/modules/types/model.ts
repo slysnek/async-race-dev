2df562c48135180ee586b682fb180fb52fb9ccaf
Kirill <bloodwingest@gmail.com>
2023-01-22 22:07:01 +0300

    feat: add interface for model

--- async-race/src/modules/types/model.ts
+++ async-race/src/modules/types/model.ts
@@ -1,3 +1,9 @@
+import {EngineMethods} from '../jsonMethods/engineMethods'
+import {GarageMethods} from '../jsonMethods/garageMethods'
+import {WinnersMethods} from '../jsonMethods/winnersMethods'
+
 export interface Model {
-  
+  engineAPI: EngineMethods
+  garageAPI: GarageMethods
+  winnersAPI: WinnersMethods
 }
