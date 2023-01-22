2f7ca3b38fbc6d64ec92400007b3f54e25dcd12d
Kirill <bloodwingest@gmail.com>
2023-01-22 16:03:45 +0300

    refactor: change structure of project

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,16 +1,4 @@
 import './main.scss';
 import './fonts/fonts.scss'
-import car from './images/dice.png'
 
 
-function component(text: string | null) {
-  const element = document.createElement('h1');
-  element.textContent = text;
-  return element;
-}
-
-const imgDiv = document.createElement('img');
-imgDiv.src = car;
-document.body.append(imgDiv);
-
-document.body.prepend(component('Hello World'));
