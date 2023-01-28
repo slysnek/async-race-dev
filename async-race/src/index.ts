45059c2888f808297a8d78c4a3ef4219ce96036e
Kirill <bloodwingest@gmail.com>
2023-01-28 13:38:14 +0300

    feat: add style to buttons, delete alert message

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -3,7 +3,6 @@ import './fonts/fonts.scss'
 import { GarageView } from './modules/garage/garageView'
 const garageView = new GarageView(document.body);
 
-//alert(`Привет! Я немного не успел доделать - осталось допилить функционал гонок и таблицы победителей. Из-за основной работы совсем не хватает времени, поэтому проверь, пожалуйста, в последний день кроссчека. Спасибо!`)
 
 garageView.addGarageElements()
 garageView.addWinnersElements()
